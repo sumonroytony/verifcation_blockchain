16067ede9f4da9ffb6815d0c7720c5bb7e270cea
tony <sumonroytony@gmail.com>
2021-09-20 15:00:23 +0600

    "modify verify.sol"

--- frontend/src/binance/verify.js
+++ frontend/src/binance/verify.js
@@ -1,8 +1,8 @@
-import web3 from './web3';
-import verifyJson from './build/Verify.json';
+import web3 from "./web3";
+import verifyJson from "./build/Verify.json";
 const verify = new web3.eth.Contract(
   JSON.parse(verifyJson.interface),
-  '0xD5D4c3d90162c632B0ba15BCF114107Ad6CBCD49'
+  "0xfa3c5886B3c07Ae85746859d4FdFBC1c0695D03a"
 );
 
 export default verify;
