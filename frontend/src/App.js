d7cb090775e8f83dab7a502ccb37f88d727da40f
tony <tony@workspaceit.com>
2021-10-01 11:53:58 +0600

    "simple change"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -183,8 +183,6 @@ function App() {
             var a = document.createElement("a");
             a.href = `/download/${pdfId}.pdf`;
             a.download = "download";
-            toast.success(`${a.href}`);
-
             a.click();
           } else {
             setLoading(false);
